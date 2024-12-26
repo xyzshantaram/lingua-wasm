b0e5a5203ae5784452922d77750b54678316e0ab
Siddharth Singh <me@shantaram.xyz>
2024-12-26 20:00:33 +0530

    add confidence values back to API

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use js_sys::Map;
+
 use lazy_static::lazy_static;
 use lingua::{LanguageDetector, LanguageDetectorBuilder};
 use wasm_bindgen::prelude::*;
@@ -11,11 +13,13 @@ lazy_static! {
 }
 
 #[wasm_bindgen]
-pub fn detect(s: &str) -> Option<String> {
-    DETECTOR
-        .detect_language_of(s)
-        .map(|v| v.iso_code_639_3().to_string())
+pub fn detect(s: &str, threshold: Option<f64>) -> Map {
     /*     DETECTOR
+    .detect_language_of(s)
+    .map(|v| v.iso_code_639_3().to_string()) */
+    let m = Map::new();
+    let t = threshold.unwrap_or(80f64);
+    DETECTOR
         .compute_language_confidence_values(s)
         .into_iter()
         .map(|(language, confidence)| {
@@ -33,5 +37,5 @@ pub fn detect(s: &str) -> Option<String> {
             }
         });
 
-    m */
+    m
 }
